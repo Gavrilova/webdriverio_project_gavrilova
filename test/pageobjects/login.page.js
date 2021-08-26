22a2b64bc2a6972d968634188ba09a6ad4ce87b2
Gavrilova <gavrilova.irina@gmail.com>
2021-08-26 00:49:42 -0500

    example.e2e.js: added test for invalid username; added test for invalid password;

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -10,9 +10,10 @@ class LoginPage extends Page {
     get inputUsername () { return $('#username') }
     get inputPassword () { return $('#password') }
     get btnSubmit () { return $('button[type="submit"]') }
+    get flashAlert() {return $('#flash')}
 
     /**
-     * a method to encapsule automation code to interact with the page
+     * a method to encapsulate automation code to interact with the page
      * e.g. to login using username and password
      */
     async login (username, password) {
@@ -22,7 +23,7 @@ class LoginPage extends Page {
     }
 
     /**
-     * overwrite specifc options to adapt it to page object
+     * overwrite specific options to adapt it to page object
      */
     open () {
         return super.open('login');
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -10,6 +10,22 @@ describe('My Login application', () => {
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
     });
+    it("shouldn't login with invalid username", async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmit1', 'SuperSecretPassword!');
+        await expect(LoginPage.flashAlert).toBeExisting();
+        await expect(LoginPage.flashAlert).toHaveTextContaining(
+            'Your username is invalid!');
+    });
+    it("shouldn't login with invalid password", async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'uperSecretPassword!');
+        await expect(LoginPage.flashAlert).toBeExisting();
+        await expect(LoginPage.flashAlert).toHaveTextContaining(
+            'Your password is invalid!');
+    });
 });
 
 
