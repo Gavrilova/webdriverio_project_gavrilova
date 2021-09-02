deaaa22435f7d814fade7d5ccb5a93980c9917ec
Gavrilova <gavrilova.irina@gmail.com>
2021-09-01 20:03:12 -0500

    1) basic_auth.page.js: added isDisplay method; 2) test_basic_auth.js: make tests not skipped, change selectors; added extracted function to enter keys in basic auth scheme;

--- test/pageobjects/basic_auth.page.js
+++ test/pageobjects/basic_auth.page.js
@@ -9,19 +9,46 @@ class BasicAuthPage extends Page {
      */
 
     get getHeader() {
-        return browser.$('#h3');
+        return browser.$('h3'); //css selector $('h3')
     }
 
     get getCongratulations() {
-        return browser.$('=Congratulations');
+        return browser.$('div p');//$('*=Congratulations!');
     }
 
     get getNotAuthorized() {
         return browser.$('=Not authorized');
     }
 
+    async isDisplayed(selector) {
+        let result;
+        try {
+            await browser.$(selector);
+            result = true;}
+        catch(error)
+            {
+                console.error(error);
+                result = false;
+            }
+        return result;
+    }
+
     openBasicAuth(username, password) {
-        return super.open(username, password, "basic_auth");
+        return super.openBasicAuth(username, password, "basic_auth");
+    }
+
+    async extracted(username, password) {
+        await BasicAuthPage.openBasicAuth(username, password);
+        if (!await BasicAuthPage.isDisplayed("div p")) {  //css selector $('div p')
+            if (!await BasicAuthPage.isDisplayed("..")) { //xPath $('//body') or $('..')
+                await browser.sendKeys(username);
+                await browser.keys("\uE004");
+                await browser.sendKeys(password);
+                await browser.keys("\uE007");
+                expect(browser.getUrl).toHaveUrlContaining(urlBasicAuth);
+                expect(BasicAuthPage.getNotAuthorized).toBeDisplayed();
+            }
+        }
     }
 }
 
--- test/specs/test_basic_auth.js
+++ test/specs/test_basic_auth.js
@@ -1,19 +1,36 @@
 const BasicAuthPage = require('../pageobjects/basic_auth.page');
 
-describe('basic authentication functionality', () => {
-        it('should basic auth login with basic credentials'), async () => {
-            await BasicAuthPage.openBasicAuth("admin", "admin");
-            await expect(BasicAuthPage.getCongratulations).toBeExisting();
-            await expect(BasicAuthPage.getHeader).toHaveText("Basic Auth");
+async function extracted(username, password, urlBasicAuth) {
+    await BasicAuthPage.openBasicAuth(username, password);
+    if (!await BasicAuthPage.isDisplayed("div p")) {  //css selector $('div p')
+        if (!await BasicAuthPage.isDisplayed("..")) { //xPath $('//body') or $('..')
+            await browser.sendKeys(username);
+            await browser.keys("\uE004");
+            await browser.sendKeys(password);
+            await browser.keys("\uE007");
+            expect(browser.getUrl).toHaveUrlContaining(urlBasicAuth);
+            expect(BasicAuthPage.getNotAuthorized).toBeDisplayed();
         }
+    }
+}
 
-    it('should basic auth login with invalid username'), async () => {
-        await BasicAuthPage.openBasicAuth("admiM", "admin");
-        await expect(BasicAuthPage.getNotAuthorized).toBeExisting();
-    }
-    it('should basic auth login with invalid password'), async () => {
-        await BasicAuthPage.openBasicAuth("admin", "admiM");
-        await expect(BasicAuthPage.getNotAuthorized).toBeExisting();
-    }
+describe('basic authentication functionality', () => {
+        const urlBasicAuth = "https://the-internet.herokuapp.com/basic_auth";
+        it('should basic auth login with valid credentials', async () => {
+            await BasicAuthPage.openBasicAuth("admin", "admin");
+            await driver.waitUntil((BasicAuthPage.getHeader).isDisplayed);
+            await expect(BasicAuthPage.getCongratulations).toHaveTextContaining("Congratulations!");
+            await expect(BasicAuthPage.getHeader).toHaveText("Basic Auth");
+            expect(browser.getUrl).toHaveUrlContaining(urlBasicAuth);
+        });
+
+        it("shouldn't basic auth login with invalid username", async () => {
+
+            await extracted("admiM", "admin", urlBasicAuth);
+        });
+
+        it("shouldn't basic auth login with invalid password", async () => {
+            await extracted("admin", "admiM", urlBasicAuth);
+        });
     }
 )
